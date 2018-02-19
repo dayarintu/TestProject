587a216f7bfdc7f60802faa05ca231afb61a7510
dhaya <dhayareddy89@gmail.com>
2018-02-19 18:16:47 -0500

    Initial Commit

---
+++ Variables/src/Variable/MyVariables.java
@@ -0,0 +1,18 @@
+package Variable;
+
+public class MyVariables {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int c;
+		
+		a = 6;
+		b = 7;
+		c = a + b;
+		
+      System.out.println("The sum is : " + c);		
+
+	}
+
+}
